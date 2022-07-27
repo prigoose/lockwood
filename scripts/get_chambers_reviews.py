3e70b985aa7e406bf916c37b4491eab794214f16
prigoose <priyalghose@gmail.com>
2022-07-27 10:32:25 -0400

    saving my work

--- scripts/get_chambers_reviews.py
+++ scripts/get_chambers_reviews.py
@@ -1,28 +1,25 @@
 import json
 
-import numpy as np
 import pandas as pd
 import requests
 
 
 def get_chambers_reviews():
     # open data from csv
-    df = pd.read_csv(r"firms_clean.csv")
+    df = pd.read_csv("data/people.csv")
     results = []
 
-    df_rg_lawyers = df.query("organisationId == 3650", engine="python")
+    # ropes & gray = 3650
+    # kirkland & ellis = 3636
+    df_org_lawyers = df.query("organisationId == 3636", engine="python")
 
-    # rg_lawyers = df_rg_lawyers.personOrganisationId.dropna().unique()
-
-    # all_lawyers = df.personOrganisationId.dropna().unique()
-    rg_lawyers = df_rg_lawyers.drop_duplicates(subset="personOrganisationId").dropna(
+    org_lawyers = df_org_lawyers.drop_duplicates(subset="personOrganisationId").dropna(
         subset="personOrganisationId"
     )
-    rg_lawyers = rg_lawyers.reset_index()
+    org_lawyers = org_lawyers.reset_index()
 
-    print("length: ", len(rg_lawyers))
-    # for index, id in np.ndenumerate(rg_lawyers):
-    for index, row in rg_lawyers.iterrows():
+    print("length: ", len(org_lawyers))
+    for index, row in org_lawyers.iterrows():
         try:
             print("index: ", index)
             response = requests.get(
@@ -30,16 +27,18 @@ def get_chambers_reviews():
             )
             if response.status_code == 204:  # if there's no data on this id, just skip
                 continue
-            lawyer = response.json()
-            lawyer[0]["name"] = row["personName"]
-            lawyer[0]["image"] = row["profilePictureUrl"]
-            results += lawyer
+            lawyers = response.json()
+            for lawyer in lawyers:
+                lawyer["personOrganisationId"] = row["personOrganisationId"]
+                lawyer["name"] = row["personName"]
+                lawyer["image"] = row["profilePictureUrl"]
+            results += lawyers
 
         except Exception as e:
             print("hit exception: ", e)
             breakpoint()
 
-    with open("bandit/bandit/chambers_reviews_rg.json", "w") as reviewsjson:
+    with open("data/chambers_reviews_kirkland_ellis.json", "w") as reviewsjson:
         json.dump(results, reviewsjson, indent=4)
         print(f"data dumped into json file")
 
---
+++ scripts/individual_rankings.py
@@ -0,0 +1,6 @@
+# I didn't write a script to pull individual rankings yet
+# Instead, I went to the page of rankings I wanted, opened my console to the Network tab,
+# and just copied and pasted the response from 
+# https://api.chambers.com/api/subsections/<id>/individual-rankings?multi-language=true
+# into a json file
+
